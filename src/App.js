599b146d63e4eefb8278efdef8627894afaa4a99
Jonathan Chan <johnkchan252@gmail.com>
2020-01-25 18:43:55 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
 // create your App component here
+import React, { Component } from 'react';
+
+class BlogContent extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.articleText}
+      </div>  
+    )
+  }
+}
